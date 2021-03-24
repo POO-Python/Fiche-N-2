8915de901f5926efa670034169785ede8c167132
ClemLcs <locascio.clement@gmail.com>
2021-03-24 10:06:55 +0100

    Exercice 2

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ def main() :
         return sqrt(x) / (x - 2)
 
     try:
-        saisie = float(input("Saisir un nombre x: "))
+        saisie = float(input("Veuillez Saisir un nombre x: "))
         print(calcul(saisie))
     except ValueError: 
         print("Veuillez entrer un nombre positif.")
         main()
     except ZeroDivisionError:
         print("Vous avez saisie un nombre effectuant une division par 0.")
-        print("Veuillez recommencer.")
         main()
     except Exception:
         print("Une erreur c'est produite.")
